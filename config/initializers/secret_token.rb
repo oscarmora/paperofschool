7175889c6f75dfdd62ac10f21c3536acce4f8acd
Oscar Mora <oamora1@misena.edu.co>
2013-04-29 00:35:41 -0500

    git update index

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Paperofschool::Application.config.secret_token = '00370b8d048951eba6a8bc85f79fca1d601659e71ca2b740b86ba9de1e77a030f7e3b15bf599d42e809da8bae48c2432d67d43d4fe82540ba35e9676ebd73648'
